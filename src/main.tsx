cfdde91448d36cda1b4f724414aa5d5e871749e6
praveenR <praveen.r@deventure.co>
2024-12-27 19:14:28 +0530

    added rsuite

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from "react-dom/client";
+import "rsuite/dist/rsuite.min.css";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "@mui/styles";
 import theme from "./styles/theme.tsx";
+import { CustomProvider } from "rsuite";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <CustomProvider>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </CustomProvider>
 );
--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -1,9 +1,15 @@
 import { Container } from "@mui/material";
+import { Button } from "rsuite";
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
-  return <Container>Dashboard</Container>;
+  return (
+    <Container>
+      Dashboard
+      <Button appearance="primary">Primary</Button>
+    </Container>
+  );
 };
 
 export default Dashboard;
