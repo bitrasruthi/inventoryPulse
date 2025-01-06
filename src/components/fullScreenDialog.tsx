73963baff2079ecbd05e59cbeb31ec4ace04e286
praveen_minion_1998 <praveen@3analytics.com>
2025-01-06 17:58:23 +0530

    dialog component fix

--- src/components/fullScreenDialog.tsx
+++ src/components/fullScreenDialog.tsx
@@ -1,41 +1,27 @@
-import React from "react";
-import Dialog from "@mui/material/Dialog";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Slide from "@mui/material/Slide";
-import { TransitionProps } from "@mui/material/transitions";
-import { Box, DialogActions, DialogContent, DialogTitle } from "@mui/material";
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import React from 'react';
+import Dialog from '@mui/material/Dialog';
+import Button, { ButtonProps } from '@mui/material/Button';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import { DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import DialogTitleCommon from './dialogTitleCommon';
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & { children: React.ReactElement },
   ref: React.Ref<unknown>
 ) {
-  return <Slide direction="left" ref={ref} {...props} />;
+  return <Slide direction='left' ref={ref} {...props} />;
 });
 
-// type ButtonColor =
-//     | "inherit"
-//     | "primary"
-//     | "secondary"
-//     | "success"
-//     | "error"
-//     | "info"
-//     | "warning";
-
-type ButtonProps = {
+export interface CustomButtonProps extends ButtonProps {
   label: string;
-  onClick: () => void;
-  // color?: string | ButtonColor;
-};
+}
 
 type FullScreenDialogProps = {
   open: boolean;
   onClose: () => void;
   title: string;
-  buttons: ButtonProps[];
+  buttons: CustomButtonProps[];
   children: React.ReactNode;
 };
 
@@ -49,60 +35,30 @@ const FullScreenDialog: React.FC<FullScreenDialogProps> = ({
   return (
     <Dialog
       fullScreen
-      open={open}
+      open={true}
       onClose={onClose}
       TransitionComponent={Transition}
       sx={{
-        display: "flex",
-        flexDirection: "column",
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
-      <DialogTitle sx={{ position: "relative" }}>
-        <Toolbar>
-          <IconButton edge="start">
-            <ArrowBackIosNewIcon
-              onClick={onClose}
-              aria-label="close"
-              fontSize="small"
-              sx={{
-                color: "white",
-                backgroundColor: "#8542E9",
-                borderRadius: 1,
-              }}
-            />
-          </IconButton>
-
-          <Typography sx={{ ml: 2, flex: 1, fontWeight: "bold" }}>
-            {title}
-          </Typography>
-        </Toolbar>
+      <DialogTitle sx={{ display: 'flex', alignItems: 'center', px: 4 }}>
+        <DialogTitleCommon title={title} onClose={onClose} />
       </DialogTitle>
-      <DialogContent sx={{ flex: 1, overflowY: "auto" }}>
-        <Typography variant="body1" sx={{ p: 2 }}>
-          {children}
-        </Typography>
+      <DialogContent sx={{ flex: 1, overflowY: 'auto', p: 2 }}>
+        {children}
       </DialogContent>
       <DialogActions
         sx={{
-          position: "sticky",
-          bottom: 0,
-          background: "white",
-          display: "flex",
+          position: 'sticky',
+          px: 4,
+          display: 'flex',
           gap: 2,
-          p: 2,
         }}
       >
         {buttons.map((button, index) => (
-          <Button
-            key={index}
-            onClick={button.onClick}
-            // color={button.color || "primary"}
-            sx={{
-              border: "1px solid #ccc",
-              borderRadius: 1,
-              padding: "8px 16px",
-            }}
-          >
+          <Button key={index} onClick={button.onClick} {...button}>
             {button.label}
           </Button>
         ))}
--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
-import { Container, Typography } from "@mui/material";
-import { Button } from "rsuite";
-import FullScreenDialog from "../../components/fullScreenDialog";
+import React, { useState } from 'react';
+import {
+  ButtonBaseProps,
+  ButtonProps,
+  Container,
+  Typography,
+} from '@mui/material';
+import { Button } from 'rsuite';
+import FullScreenDialog, {
+  CustomButtonProps,
+} from '../../components/fullScreenDialog';
 
 const Dashboard = () => {
   const [dialogOpen, setDialogOpen] = useState(false);
@@ -15,49 +22,97 @@ const Dashboard = () => {
   };
 
   const handleAgree = () => {
-    console.log("Agree clicked");
+    console.log('Agree clicked');
     setDialogOpen(false);
   };
 
   const handleDisagree = () => {
-    console.log("Disagree clicked");
+    console.log('Disagree clicked');
     setDialogOpen(false);
   };
 
-  const actionButtons = [
-    { label: "Disagree", onClick: handleDisagree },
-    { label: "Agree", onClick: handleAgree },
-  ];
+  // const actionButtons = [
+  //   { label: 'Disagree', onClick: handleDisagree, variant: 'contained' },
+  //   { label: 'Agree', onClick: handleAgree },
+  // ] as ButtonProps;
 
   return (
     <Container>
       <h1>Dashboard</h1>
-      <Button appearance="primary" onClick={handleOpenDialog}>
+      <Button appearance='primary' onClick={handleOpenDialog}>
         Primary
       </Button>
       <FullScreenDialog
         open={dialogOpen}
         onClose={handleCloseDialog}
-        title="Add Inspection"
-        buttons={actionButtons}
+        title='Add Inspection'
+        buttons={
+          [
+            {
+              label: 'Cancel',
+              variant: 'outlined',
+              onClick: () => console.log('canceled'),
+            },
+            {
+              label: 'Confirm',
+              variant: 'contained',
+              onClick: () => console.log('Confirm'),
+            },
+          ] as CustomButtonProps[]
+        }
       >
-        <Typography variant="body1" sx={{ p: 2 }}>
-          This is the content of the dialog passed as children.
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
-          To make Typography bold in Material-UI, the fontWeight property must be applied correctly. However, when you're using the sx prop, you don't need to add !important like in plain CSS because sx already has higher specificity
+        <Typography variant='body1' sx={{ p: 2 }}>
+          This is the content of the dialog passed as children. To make
+          Typography bold in Material-UI, the fontWeight property must be
+          applied correctly. However, when you're using the sx prop, you don't
+          need to add !important like in plain CSS because sx already has higher
+          specificity To make Typography bold in Material-UI, the fontWeight
+          property must be applied correctly. However, when you're using the sx
+          prop, you don't need to add !important like in plain CSS because sx
+          already has higher specificity To make Typography bold in Material-UI,
+          the fontWeight property must be applied correctly. However, when
+          you're using the sx prop, you don't need to add !important like in
+          plain CSS because sx already has higher specificity To make Typography
+          bold in Material-UI, the fontWeight property must be applied
+          correctly. However, when you're using the sx prop, you don't need to
+          add !important like in plain CSS because sx already has higher
+          specificity To make Typography bold in Material-UI, the fontWeight
+          property must be applied correctly. However, when you're using the sx
+          prop, you don't need to add !important like in plain CSS because sx
+          already has higher specificity To make Typography bold in Material-UI,
+          the fontWeight property must be applied correctly. However, when
+          you're using the sx prop, you don't need to add !important like in
+          plain CSS because sx already has higher specificity To make Typography
+          bold in Material-UI, the fontWeight property must be applied
+          correctly. However, when you're using the sx prop, you don't need to
+          add !important like in plain CSS because sx already has higher
+          specificity To make Typography bold in Material-UI, the fontWeight
+          property must be applied correctly. However, when you're using the sx
+          prop, you don't need to add !important like in plain CSS because sx
+          already has higher specificity To make Typography bold in Material-UI,
+          the fontWeight property must be applied correctly. However, when
+          you're using the sx prop, you don't need to add !important like in
+          plain CSS because sx already has higher specificity To make Typography
+          bold in Material-UI, the fontWeight property must be applied
+          correctly. However, when you're using the sx prop, you don't need to
+          add !important like in plain CSS because sx already has higher
+          specificity To make Typography bold in Material-UI, the fontWeight
+          property must be applied correctly. However, when you're using the sx
+          prop, you don't need to add !important like in plain CSS because sx
+          already has higher specificity To make Typography bold in Material-UI,
+          the fontWeight property must be applied correctly. However, when
+          you're using the sx prop, you don't need to add !important like in
+          plain CSS because sx already has higher specificity To make Typography
+          bold in Material-UI, the fontWeight property must be applied
+          correctly. However, when you're using the sx prop, you don't need to
+          add !important like in plain CSS because sx already has higher
+          specificity To make Typography bold in Material-UI, the fontWeight
+          property must be applied correctly. However, when you're using the sx
+          prop, you don't need to add !important like in plain CSS because sx
+          already has higher specificity To make Typography bold in Material-UI,
+          the fontWeight property must be applied correctly. However, when
+          you're using the sx prop, you don't need to add !important like in
+          plain CSS because sx already has higher specificity
         </Typography>
       </FullScreenDialog>
     </Container>
