5ecedb0577a0272e8714a453ac2315b87c4f181a
kumutha <kumutha.c@3analytics.com>
2025-01-17 17:51:44 +0530

    inspection status stepper

--- src/pages/Inspections/InspectionDetails/index.tsx
+++ src/pages/Inspections/InspectionDetails/index.tsx
@@ -5,55 +5,59 @@ import { inspectionsDetailsMenuList } from "../../../constants/constants";
 import GradientButton from "../../../components/gradientButton";
 import IconButton from "@mui/material/IconButton";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import InspectionStatus from "./inspectionStatusStepper";
 
 function InspectionDetails() {
   const theme = useTheme();
   const [activeTab, setActiveTab] = useState<string>(
     inspectionsDetailsMenuList[0]?.value
   );
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        position: "relative",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          //   gap: 1,
-        }}
-      >
-        <IconButton
+    return (
+      <>
+        <Box
           sx={{
-            marginLeft: 2,
-            color: "white",
-            backgroundColor: theme.palette.primary.main,
-            borderRadius: 1.5,
-            "&:hover": {
-              backgroundColor: theme.palette.primary.main,
-            },
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            position: "relative",
           }}
         >
-          <ArrowBackIosNewIcon aria-label="close" fontSize="medium" />
-        </IconButton>
-        <TabsCommon
-          tabMenuList={inspectionsDetailsMenuList}
-          onTabChange={(value) => setActiveTab(value)}
-          tabContent={<></>}
-        />
-      </Box>
-      <GradientButton
-        label="Mark As Complete"
-        // menuList={inspectionBtnList}
-        isGradient={true}
-        // handleDialogOpen={handleOpenDialog}
-      />
-    </Box>
-  );
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              //   gap: 1,
+            }}
+          >
+            <IconButton
+              sx={{
+                marginLeft: 2,
+                color: "white",
+                backgroundColor: theme.palette.primary.main,
+                borderRadius: 1.5,
+                "&:hover": {
+                  backgroundColor: theme.palette.primary.main,
+                },
+              }}
+            >
+              <ArrowBackIosNewIcon aria-label="close" fontSize="medium" />
+            </IconButton>
+            <TabsCommon
+              tabMenuList={inspectionsDetailsMenuList}
+              onTabChange={(value) => setActiveTab(value)}
+              tabContent={<></>}
+            />
+          </Box>
+          <GradientButton
+            label="Mark As Complete"
+            // menuList={inspectionBtnList}
+            isGradient={true}
+            // handleDialogOpen={handleOpenDialog}
+          />
+        </Box>
+        <InspectionStatus />
+      </>
+    );
 }
 
 export default InspectionDetails;
---
+++ src/pages/Inspections/InspectionDetails/inspectionStatusStepper.tsx
@@ -0,0 +1,102 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepContent from "@mui/material/StepContent";
+import Typography from "@mui/material/Typography";
+import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined"; // Empty circle icon
+import CheckCircleIcon from "@mui/icons-material/CheckCircle"; // Checkmark icon (optional)
+import ContentWrapper from "../../../components/contentWrapper";
+
+const steps = [
+  {
+    label: "Pending",
+    description: "04 Jan",
+    value: 1,
+  },
+  {
+    label: "Assigned",
+    description: "04 Jan",
+    value: 2,
+  },
+  {
+    label: "Active",
+    description: "04 Jan",
+    value: 3,
+  },
+  {
+    label: "Complete",
+    description: "04 Jan",
+    value: 4,
+  },
+  {
+    label: "Closed",
+    description: "04 Jan",
+    value: 5,
+  },
+];
+
+// Custom StepIcon component
+function CustomStepIcon({
+  active,
+  completed,
+}: {
+  active: boolean;
+  completed: boolean;
+}) {
+  if (completed) {
+    return <CheckCircleIcon color="primary" />;
+  }
+  return active ? (
+    <CircleOutlinedIcon color="primary" />
+  ) : (
+    <CircleOutlinedIcon color="disabled" />
+  );
+}
+
+function InspectionStatusStepper() {
+  const [activeStep] = React.useState(3);
+
+  return (
+    <ContentWrapper>
+      <Box sx={{ Width: "100%" }}>
+        <Stepper activeStep={activeStep - 1} orientation="vertical">
+          {steps.map((step, index) => (
+            <Step key={step.label} completed={activeStep > step.value}>
+              <StepLabel
+                sx={{
+                  "& .MuiStepLabel-label": {
+                    fontWeight: activeStep === step.value ? "bold" : "normal",
+                    color:
+                      activeStep === step.value
+                        ? (theme) => theme.palette.primary.main
+                        : "inherit", 
+                  },
+                }}
+                slots={{
+                  stepIcon: (props) =>
+                    React.cloneElement(
+                      <CustomStepIcon active={false} completed={false} />,
+                      {
+                        active: activeStep === step.value,
+                        completed: activeStep > step.value,
+                      }
+                    ),
+                }}
+              >
+                {step.label}
+              </StepLabel>
+
+              <StepContent>
+                <Typography>{step.description}</Typography>
+              </StepContent>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+    </ContentWrapper>
+  );
+}
+
+export default InspectionStatusStepper;
