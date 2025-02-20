ab6cebe9d00fe25047f6b4c2c6f11488093252c0
praveenR <praveen.r@deventure.co>
2025-02-20 17:08:39 +0530

    added edit icon

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Container } from "@mui/material";
+import EditIcon from "../../assets/icons/editIcon";
 
 const Dashboard = () => {
   return (
     <Container>
       <h1>Dashboard</h1>
+      <EditIcon />
     </Container>
   );
 };
