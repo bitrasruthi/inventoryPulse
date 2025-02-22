7dd46a39518b3290bec620bce4f2a052ed210899
praveenR <praveen.r@deventure.co>
2025-02-22 20:55:36 +0530

    added react query swagger api generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import CustomSnackBar from "./components/customSnackbar";
 import SnackBarProvider from "./context/SnackBarContext";
 import MainRouter from "./routes/mainRouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 2,
+      refetchOnMount: false,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
       <SnackBarProvider>
         <MainRouter />
         <CustomSnackBar />
       </SnackBarProvider>
-    </>
+    </QueryClientProvider>
   );
 }
 
