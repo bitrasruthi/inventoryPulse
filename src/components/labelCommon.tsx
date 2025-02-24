e671a1719f3b8fc0dc93fa9dd3cf6af35715f508
Sruthi <bitrabhagyasruthi@gmail.com>
2025-02-24 20:43:37 +0530

    report settings page UI added

--- src/components/labelCommon.tsx
+++ src/components/labelCommon.tsx
@@ -1,22 +1,37 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import InfoIconWithTooltip from "./infoIconWithTooltip";
 
 interface IProps {
   fieldName: React.ReactNode;
   isRequired?: boolean;
   fontSize?: string;
   fontFamily?: string;
+  hasInfoIcon?: boolean;
 }
 
 const LabelCommon: React.FC<IProps> = (props) => {
-  const { fieldName, isRequired, fontSize = "16px", fontFamily } = props;
+  const {
+    fieldName,
+    isRequired,
+    fontSize = "16px",
+    fontFamily,
+    hasInfoIcon,
+  } = props;
   return (
-    <Typography
-      sx={{ fontSize: fontSize, fontFamily: fontFamily || "roboto-regular" }}
-    >
-      {fieldName}
-      {isRequired && <span style={{ color: "red" }}> * </span>}
-    </Typography>
+    <Box display={"flex"} alignItems={"center"}>
+      <Typography
+        sx={{
+          fontSize: fontSize,
+          fontFamily: fontFamily || "roboto-regular",
+          pr: 1,
+        }}
+      >
+        {fieldName}
+        {isRequired && <span style={{ color: "red" }}> * </span>}
+      </Typography>
+      {hasInfoIcon && <InfoIconWithTooltip title={"Tooltip text for info"} />}
+    </Box>
   );
 };
 
